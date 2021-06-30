bbd8662463a09fbca0e1688e81b117ea0173510b
Leon Heess <leon@heess.me>
2021-07-01 00:46:10 +0200

    Add support for special characters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import daemon
+#import daemon
 import dotenv
 import telebot
 from datetime import datetime
@@ -15,56 +15,68 @@ def get_destination_url(url):
   return (requests.head(
     url,
     allow_redirects=True,
-    headers={"User-Agent": "Mozilla/5.0 (Linux; Android 11; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36"},
+    headers={
+      "User-Agent": "Mozilla/5.0 (Linux; Android 11; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36"},
   )).url
 
-with daemon.DaemonContext():
-  dotenv.load_dotenv()
-  bot = telebot.TeleBot(os.getenv('API_KEY'))
+def formatUrl(url):
+  url = url[:-1] if url.endswith('/') else url
+  return url\
+    .replace("_", "\_")\
+    .replace("*", "\*")\
+    .replace("[", "\[")\
+    .replace("`", "\`")\
 
-  @bot.message_handler(commands=['start'])
-  def send_start(message):
-    bot.reply_to(message, START)
+# with daemon.DaemonContext():
+dotenv.load_dotenv()
+bot = telebot.TeleBot(os.getenv('API_KEY'))
 
-  @bot.message_handler(commands=['help'])
-  def send_help(message):
-    bot.reply_to(message, HELP)
+@bot.message_handler(commands=['start'])
+def send_start(message):
+  bot.reply_to(message, START)
 
-  @bot.message_handler(func=lambda message: True)
-  def echo_all(message):
-    text = message.text.strip()
-    reply = ERROR
+@bot.message_handler(commands=['help'])
+def send_help(message):
+  bot.reply_to(message, HELP)
 
-    if ' ' in text or '.' not in text:
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+  text = message.text.strip()
+
+  if ' ' in text or '.' not in text:
+    reply = INVALID
+  else:
+    try:
+      if text.startswith('https://open.spotify.com') or text.startswith('https://www.instagram.com'):
+        url = text
+      else:
+        text = text if text.startswith('http') else 'http://' + text
+        url = get_destination_url(text)
+
+      if url.startswith('https://www.amazon'):
+        url = url.split('/ref')[0]
+      elif 'youtube.com' in url:
+        url = 'https://m.youtube.com/watch?v=' + text.split('/')[3]
+      else:
+        url = url.split('?')[0]
+
+      formatted_url = formatUrl(url)
+      reply = f'{formatted_url}\n\n\nTap here to copy:\n\n`{formatted_url}`'
+    except requests.exceptions.MissingSchema:
       reply = INVALID
-    else:
-      try:
-        if text.startswith('https://open.spotify.com') or text.startswith('https://www.instagram.com'):
-          url = text
-        else:
-          text = text if text.startswith('http') else 'http://' + text
-          url = get_destination_url(text)
+    except requests.ConnectionError:
+      reply = UNREACHABLE
+    except BaseException:
+      reply = ERROR
 
-        if url.startswith('https://www.amazon'):
-          url = url.split('/ref')[0]
-        elif 'youtube.com' in url:
-          url = 'https://m.youtube.com/watch?v=' + text.split('/')[3]
-        else:
-          url = url.split('?')[0]
-
-        url = url[:-1] if url.endswith('/') else url
-        reply = f'{url}\n\nTap here to copy: `{url}`'
-      except requests.exceptions.MissingSchema:
-        reply = INVALID
-      except requests.ConnectionError:
-        reply = UNREACHABLE
-      except BaseException:
-        reply = ERROR
-
+  try:
     bot.reply_to(message, reply, disable_web_page_preview=True, parse_mode='Markdown')
+  except BaseException:
+    bot.reply_to(message, ERROR)
 
-    if str(message.chat.id) != os.getenv('USER_ID'):
-      date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-      bot.send_message(os.getenv('LOG_ID'), f'*Time:* {date}\n\n*Message:* `{text}`\n\n*Response:* {reply}', disable_web_page_preview=True, parse_mode='Markdown')
+  if str(message.chat.id) != os.getenv('USER_ID'):
+    date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    log_message = f'*Time:* {date}\n\n*Message:* `{text}`\n\n*Response:* {reply}'
+    bot.send_message(os.getenv('LOG_ID'), log_message , disable_web_page_preview=True, parse_mode='Markdown')
 
-  bot.polling()
+bot.polling()
